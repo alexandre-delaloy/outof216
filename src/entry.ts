ac2b247d23c046b6fbd1c33e83de5ee728580c6a
blyndusk <alexandredelaloydev@gmail.com>
2019-12-03 10:07:36 +0100

    :ok_hand: 1st view test

--- src/entry.ts
+++ src/entry.ts
@@ -1,5 +1,19 @@
-import sum from './utils/sum';
+import keyboardManager from './utils/keyboardManager';
+import TypewriterController from './Controllers/TypewriterController';
+import TypewriterModel from './Models/TypewriterModel';
+import TypewriterView from './Views/TypewriterView';
 
-sum(10, 20);
+const RANDOM_WORDS: string[] = ['italia', 'am', 'an', 'array', 'of', 'random', 'words'];
+const RANDOM_WORDS_SPLITTED: string[][] = RANDOM_WORDS.map((word: string) => {
+    return word.split('');
+});
 
-document.querySelector('body').innerHTML = 'Hello, World !';
+// tslint:disable-next-line: no-unused-expression
+const twModel = new TypewriterModel(RANDOM_WORDS_SPLITTED);
+const twView = new TypewriterView(document.querySelector('.typewriter'));
+const twController = new TypewriterController(twModel, twView);
+
+twController.updateView();
+twController.handleType();
+
+keyboardManager();
