65e71ca86e36938e076f708bc2496612be588a30
blyndusk <alexandredelaloydev@gmail.com>
2019-12-03 15:58:59 +0100

    :bug: fix all proximity bugs

--- src/Controllers/TypewriterController.ts
+++ src/Controllers/TypewriterController.ts
@@ -1,57 +1,81 @@
-import { tsConstructorType } from "@babel/types";
-
 export default class TypewriterController {
     private model: any;
     private view: any;
+    private letterIndex: number;
+    private isWordCompleted: boolean;
+    private isLetterRight: boolean;
+    private executed: boolean;
+    private executed2: boolean;
+    private hasToBeDeleted: boolean;
     constructor(model: any, view: any) {
         this.model = model;
         this.view = view;
+        this.letterIndex = 0;
+        this.isWordCompleted = false;
+        this.isLetterRight = false;
+        this.executed = false;
+        this.executed2 = false;
+        this.hasToBeDeleted = false;
     }
     public updateView() {
         return this.view.displayWords(this.model.words);
     }
-    public handleType() {
-        let i: number = 0;
-        let validated: boolean = false;
-        let executed: boolean = false;
-        return window.addEventListener('keydown', (e) => {
-            let LETTER = this.model.getWords()[0][i];
-            if (e.key === this.model.getWords()[0][i]) {
-                this.model.getWords()[0][i] = `<i style="color:lightgreen;">${LETTER}</i>`;
-                this.model.setWords(this.model.getWords());
-                i++;
-                if (i === this.model.getWords()[0].length) {
-                    validated = true;
-                }
-
-                this.updateView();
-            } else {
-               if (!validated) {
-                    if (!executed) {
-                        this.model.getWords()[0][i] = `<i style="color:red;">${LETTER}</i>`;
-                        executed = true;
-                    }
-                    this.model.setWords(this.model.getWords());
-                    
-                    if (e.code === 'Backspace') {
-                        this.model.getWords()[0][i] = LETTER.match(/>[a-z]/g).join().replace('>', '');
-                    }
-                    this.updateView();
-                }
-                
-            }
-            if (e.code === 'Space' && validated) {
-                i = 0;
-                validated = false;
-                return this.removeFirstWord();
-            } else {
-                return;
-            }
-        });
+    public stylizeLetter(type: string, i: number) {
+        switch (type) {
+            case 'right':
+                console.warn('💚 - right letter');
+                return this.model.getWords()[0][i] = `<i style="color:lightgreen;">${this.model.getWords()[0][i]}</i>`;
+            case 'wrong':
+                console.warn('❤️ - wrong letter');
+                return this.model.getWords()[0][i] = `<i style="color:red;">${this.model.getWords()[0][i]}</i>`;
+            case 'correct':
+                console.warn('💛 - corrected letter');
+                return this.model.getWords()[0][i] = this.model.getWords()[0][i]
+                    .match(/>[a-z]/g)
+                    .join()
+                    .replace('>', '');
+        }
+        return this.model.setWords(this.model.getWords());
     }
-    private removeFirstWord() {
+    public wordValidation() {
+        console.warn('💜 - word validated');
         this.model.getWords().shift();
         this.model.setWords(this.model.getWords());
-        return this.updateView();
     }
+    public letterCorrection(i: number) {
+        this.stylizeLetter('correct', i);
+    }
+    public handleType() {
+        let isWordFinished: boolean = false;
+        let hasToBeCorrected: boolean = false;
+        let i: number = 0;
+        return window.addEventListener('keydown', (e) => {
+            if (e.key === this.model.getWords()[0][i]) {
+                this.stylizeLetter('right', i);
+                i++;
+                if (i === this.model.getWords()[0].length) {
+                    console.warn('💙 - finished');
+                    isWordFinished = true;
+                }
+            } else {
+                if (e.code === 'Space') {
+                    if (isWordFinished) {
+                        this.wordValidation();
+                        isWordFinished = false;
+                        i = 0;
+                    }
+                } else if (e.code === 'Backspace') {
+                    if (!isWordFinished && hasToBeCorrected) {
+                        this.letterCorrection(i);
+                        hasToBeCorrected = false;
+                    }
+                } else {
+                    this.stylizeLetter('wrong', i);
+                    hasToBeCorrected = true;
+                }
+            }
+            return this.updateView();
+        });
+    }
+
 }
