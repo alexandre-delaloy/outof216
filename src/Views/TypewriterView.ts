dd4c8348a0bd08c40247a43c2ae0c1fff0bdbf45
blyndusk <alexandredelaloydev@gmail.com>
2019-12-04 16:42:25 +0100

    :bug: remove useless params

--- src/Views/TypewriterView.ts
+++ src/Views/TypewriterView.ts
@@ -3,7 +3,7 @@ export default class TypewriterView {
     constructor(entryNode: HTMLElement) {
         this.entryNode = entryNode;
     }
-    private display(words: string[][], goodWordsCount: number, badWordsCount: number) {
+    private display(words: string[][]) {
         return this.entryNode.innerHTML = `
             <strong>>&nbsp;</strong>${words.map((word: string[]) => {
                 return `<span>${word.join('')}</span>`;
