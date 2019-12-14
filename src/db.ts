45a5f7587513e87d85c5b24ff2f2755cae5c30a5
blyndusk <alexandredelaloydev@gmail.com>
2019-12-14 01:31:39 +0100

    :ok_hand: set firebase to prod

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,6 @@
-import * as firebase from 'firebase';
-import 'array.prototype.move';
+import * as firebase from 'firebase/app';
+import 'firebase/analytics';
+import 'firebase/database';
 
 import PodiumModel from './Models/PodiumModel';
 import PodiumView from './Views/PodiumView';
