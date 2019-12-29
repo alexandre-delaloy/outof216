1a13ac44fb8dc4467faa81618b24e4440056c642
blyndusk <alexandredelaloydev@gmail.com>
2019-12-29 18:40:14 +0100

    :construction: WIP

--- src/db.ts
+++ src/db.ts
@@ -14,86 +14,81 @@ import TypewriterController from './Controllers/TypewriterController';
 
 import { IUser } from './types';
 
+// tslint:disable-next-line: no-console
+console.log('outof216 !');
+
 declare var particlesJS: any;
+fetch('/__/firebase/init.json').then(async response => {
+    firebase.initializeApp(await response.json());
 
-fetch('http://localhost:3000/firebase')
-    .then(res => res.json())
-    .then(json => {
-        firebase.initializeApp(json);
-        firebase.analytics();
+    firebase.analytics();
+    const db = firebase.database();
 
-        const db = firebase.database();
-
-        const readData = () => {
-            const ref = db.ref('users');
-            const PARSED_USERS: any[] = [];
-            ref.on('value', (snapshot: any) => {
-                const USERS = snapshot.val();
-                for (const key in USERS) {
-                    if (USERS.hasOwnProperty(key)) {
-                        PARSED_USERS.push(USERS[key]);
-                    }
-                }
-                PARSED_USERS.sort((a, b) => {
-                    if (a.WPM < b.WPM) {
-                        return 1;
-                    }
-                    if (a.WPM > b.WPM) {
-                        return -1;
-                    }
-                    return 0;
-                });
-                const pdC = new PodiumController(
-                    new PodiumModel(PARSED_USERS),
-                    new PodiumView(document.querySelector('#podium')),
-                );
-                pdC.updateView();
-                window.scrollTo(0, 0);
-                document.querySelector('#loader').className = 'hidden';
-            });
-        };
-
-        const parseRawData = () => {
-    const RANDOM_WORDS: string[] = [];
-    let count: number = 0;
-    while (count < 100) {
-        const word = faker.random.word();
-        if (
-            !word.match(' ') &&
-            !word.match('-') &&
-            !word.match(/[A-Z]/g) &&
-            !word.match(/[0-9]/g) &&
-            word.length < 10
-        ) {
-            RANDOM_WORDS.push(word);
-            count++;
+    const readData = () => {
+    const ref = db.ref('users');
+    const PARSED_USERS: any[] = [];
+    ref.on('value', (snapshot: any) => {
+        const USERS = snapshot.val();
+        for (const key in USERS) {
+            if (USERS.hasOwnProperty(key)) {
+                PARSED_USERS.push(USERS[key]);
+            }
         }
-    }
-    const RANDOM_WORDS_SPLITTED: string[][] = RANDOM_WORDS.map((word: string) => {
-        return word.split('');
-    });
-    return RANDOM_WORDS_SPLITTED;
-};
-        const writeUserData = (user: IUser) => {
-    db.ref('users/' + Math.random().toString(36).substring(7)).set({
-        ...user,
+        PARSED_USERS.sort((a, b) => {
+            if (a.WPM < b.WPM) {
+                return 1;
+            }
+            if (a.WPM > b.WPM) {
+                return -1;
+            }
+            return 0;
         });
-    };
-        const twC = new TypewriterController(
-            new TypewriterModel(parseRawData()),
-            new TypewriterView(document.querySelector('#tw')),
-                writeUserData,
+        const pdC = new PodiumController(
+            new PodiumModel(PARSED_USERS),
+            new PodiumView(document.querySelector('#podium')),
         );
-
-        readData();
-        twC.updateView();
-        twC.handleKeys();
-
-        particlesJS.load('particles-js', 'particles.json', null);
-
-    })
-    .catch(err => {
-        throw err;
+        pdC.updateView();
+        window.scrollTo(0, 0);
+        document.querySelector('#loader').className = 'hidden';
     });
+};
 
-// firebase.initializeApp(firebaseConfig);
+    const parseRawData = () => {
+const RANDOM_WORDS: string[] = [];
+let count: number = 0;
+while (count < 100) {
+const word = faker.random.word();
+if (
+    !word.match(' ') &&
+    !word.match('-') &&
+    !word.match(/[A-Z]/g) &&
+    !word.match(/[0-9]/g) &&
+    word.length < 10
+) {
+    RANDOM_WORDS.push(word);
+    count++;
+}
+}
+const RANDOM_WORDS_SPLITTED: string[][] = RANDOM_WORDS.map((word: string) => {
+return word.split('');
+});
+return RANDOM_WORDS_SPLITTED;
+};
+    const writeUserData = (user: IUser) => {
+db.ref('users/' + Math.random().toString(36).substring(7)).set({
+...user,
+});
+};
+    const twC = new TypewriterController(
+    new TypewriterModel(parseRawData()),
+    new TypewriterView(document.querySelector('#tw')),
+        writeUserData,
+);
+
+    readData();
+    twC.updateView();
+    twC.handleKeys();
+
+    particlesJS.load('particles-js', 'particles.json', null);
+
+});
