9e9915064c624581d0a262e2e2153cb7f44b12bc
blyndusk <alexandredelaloydev@gmail.com>
2020-01-02 20:18:54 +0100

    :recycle: clean all entry code

--- src/entry.ts
+++ src/entry.ts
@@ -1,36 +1,9 @@
-import * as firebase from 'firebase/app';
-import 'firebase/analytics';
-import 'firebase/database';
-
-import faker from 'faker';
-
-import TypewriterModel from './Models/TypewriterModel';
-import TypewriterView from './Views/TypewriterView';
-import TypewriterController from './Controllers/TypewriterController';
-
-import { readData, writeData, getParsedData } from './utils';
+import { loadFirebaseData } from './utils';
 import Chart from 'chart.js';
 
 declare var particlesJS: any;
-fetch('/__/firebase/init.json').then(async response => {
-    firebase.initializeApp(await response.json());
 
-    firebase.analytics();
-    const db = firebase.database();
-
-    const twC = new TypewriterController(
-        new TypewriterModel(getParsedData(faker)),
-        new TypewriterView(
-            document.querySelector('#tw'),
-        ),
-        (user: any) => writeData(db, user),
-    );
-    readData(db);
-
-    twC.updateView();
-    twC.handleKeys();
-
-});
+loadFirebaseData();
 
 particlesJS.load('particles-js', 'particles.json', null);
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,49 +1,63 @@
-import PodiumModel from './Models/PodiumModel';
-import PodiumView from './Views/PodiumView';
-import PodiumController from './Controllers/PodiumController';
-
-import { IUser } from './types';
+import * as firebase from 'firebase/app';
+import 'firebase/analytics';
+import 'firebase/database';
+import axios from 'axios';
 
 import ChartsModel from './Models/ChartsModel';
 import chartView from './Views/ChartsView';
 import ChartController from './Controllers/ChartsController';
-
+import PodiumModel from './Models/PodiumModel';
+import PodiumView from './Views/PodiumView';
+import PodiumController from './Controllers/PodiumController';
+import TypewriterController from './Controllers/TypewriterController';
+import TypewriterModel from './Models/TypewriterModel';
+import TypewriterView from './Views/TypewriterView';
 import UserModel from './Models/UserModel';
-import UserView from './Views/UserView';
 
-declare global {
-    // tslint:disable-next-line: interface-name
-    interface Window {
-        loaded: boolean;
-    }
-}
+import { IUser } from './types';
 
-window.loaded = false;
-
-export const ph = {
-    user: {
-        name: 'John Doe',
-        WPM: 0,
-        LPS: 0,
-        words: {
-            count: 0,
-            success: 0,
-            fail: 0,
-            ratio: 0,
-        },
+export const FAKE_USER: IUser = {
+    name: 'John Doe',
+    WPM: 0,
+    LPS: 0,
+    words: {
+        count: 0,
+        success: 0,
+        fail: 0,
+        ratio: 0,
     },
 };
 
-export const readData = (db: any) => {
+export const chartsC =  new ChartController(
+    new ChartsModel(
+        new UserModel(FAKE_USER, false).getUser(),
+    ),
+    new chartView(),
+);
+
+export const loadFirebaseData = () => {
+    fetch('/__/firebase/init.json').then(async response => {
+        firebase.initializeApp(await response.json());
+        firebase.analytics();
+        const db = firebase.database();
+        getUsers(db);
+        getRandomWords(db);
+    });
+};
+
+export const getUsers = (db: any) => {
     const ref = db.ref('users');
     const PARSED_USERS: IUser[] = [];
+
     ref.on('value', (snapshot: any) => {
         const USERS = snapshot.val();
+
         for (const key in USERS) {
             if (USERS.hasOwnProperty(key)) {
                 PARSED_USERS.push(USERS[key]);
             }
         }
+
         PARSED_USERS.sort((a, b) => {
             if (a.WPM < b.WPM) {
                 return 1;
@@ -53,55 +67,58 @@ export const readData = (db: any) => {
             }
             return 0;
         });
-        const pdC = new PodiumController(
-            new PodiumModel(PARSED_USERS),
-            new PodiumView(document.querySelector('#podium')),
-        );
-        const chC =  new ChartController(
-            new ChartsModel(
-                new UserModel(ph.user, false).getUser(),
-            ),
-            new chartView(),
-        )
-        chC.displayView()
-        pdC.updateView();
-        window.scrollTo(0, 0);
-        setTimeout(() => {
-            window.loaded = true;
-           
-            document.querySelector('#loader').className = 'hidden';
-            pdC.updateChart();
-            chC.updateView();
-        }, 1000);
 
+        const podiumC = new PodiumController(
+            new PodiumModel(PARSED_USERS),
+            new PodiumView(),
+        );
+
+        chartsC.displayView();
+        podiumC.displayView();
+        window.scrollTo(0, 0);
+
+        setTimeout(() => {
+            document.querySelector('#loader').className = 'hidden';
+            podiumC.updateView();
+            chartsC.updateView();
+        }, 1000);
     });
-    return PARSED_USERS;
 };
 
-export const writeData = (db: any, user: IUser) => {
+export const getRandomWords = (db: any) => {
+    let RANDOM_WORDS: string[] = [];
+    axios.get('https://random-word-api.herokuapp.com/word?key=GG2MGY3N&number=200')
+        .then((res: any) => {
+            RANDOM_WORDS = res.data;
+            RANDOM_WORDS.forEach((el, i) => {
+                if (el.length > 10) {
+                    RANDOM_WORDS.splice(i, 1);
+                }
+            });
+
+            const RANDOM_WORDS_SPLITTED: string[][] = RANDOM_WORDS.map((word: string) => {
+                return word.split('');
+            });
+
+            initApp(RANDOM_WORDS_SPLITTED, db);
+        });
+};
+
+export const initApp = (data: string[][], db: any) => {
+    const typewriterC = new TypewriterController(
+        new TypewriterModel(data),
+        new TypewriterView(
+            document.querySelector('#tw'),
+        ),
+        (user: any) => setNewUser(db, user),
+    );
+
+    typewriterC.updateView();
+    typewriterC.handleKeys();
+};
+
+export const setNewUser = (db: any, user: IUser) => {
     db.ref('users/' + Math.random().toString(36).substring(7)).set({
         ...user,
     });
-};
-
-export const getParsedData = (faker: any) => {
-    const RANDOM_WORDS: string[] = [];
-    let count: number = 0;
-    while (count < 100) {
-        const word = faker.random.word();
-        if (
-            !word.match(' ') &&
-            !word.match('-') &&
-            !word.match(/[A-Z]/g) &&
-            !word.match(/[0-9]/g) &&
-            word.length < 10
-        ) {
-            RANDOM_WORDS.push(word);
-            count++;
-        }
-    }
-    const RANDOM_WORDS_SPLITTED: string[][] = RANDOM_WORDS.map((word: string) => {
-        return word.split('');
-    });
-    return RANDOM_WORDS_SPLITTED;
 };
