9531c0168c9817fb1dbd3b341494fb738813a8ef
blyndusk <alexandredelaloydev@gmail.com>
2020-01-12 20:21:53 +0100

    :white_check_mark: add Podium test

--- __tests__/podiumMVC.test.ts
+++ __tests__/podiumMVC.test.ts
@@ -4,17 +4,24 @@ import PodiumC from '../src/Controllers/PodiumController';
 
 import { FAKE_USER } from '../src/utils';
 
+const $podium = document.createElement('div');
+$podium.id = 'podiumChart';
+document.querySelector('body').appendChild($podium);
+
 const FAKE_USERS = [
     FAKE_USER,
     FAKE_USER,
     FAKE_USER,
-]
+];
+const _PodiumM = new PodiumM(FAKE_USERS);
 const Podium = new PodiumC(
-    new PodiumM(FAKE_USERS),
+    _PodiumM,
     new PodiumV(),
+    true,
 );
 
 test('Podium', () => {
+    _PodiumM.setUsers(FAKE_USERS);
     Podium.displayView();
     Podium.updateView();
 });
